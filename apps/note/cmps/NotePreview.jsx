7e7b0e3a5b76286e37e7726f45c6ae84a4237a38
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-13 20:58:44 +0200

    Add form to add new note

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,9 +1,40 @@
 
 export function NotePreview({ note }) {
+
+    const renderNoteContent = () => {
+        switch (note.type) {
+            case 'NoteTxt':
+                return <p>Note Content: {note.info.txt}</p>;
+            case 'NoteImg':
+                return (
+                    <React.Fragment>
+                        <p>Title: {note.info.title}</p>
+                        <p>Image Url: {note.info.url}</p>
+                        {/* <img src={note.info.url} alt={note.info.title} /> */}
+                    </React.Fragment>
+                );
+            case 'NoteTodos':
+                return (
+                    <React.Fragment>
+                        <p>Title: {note.info.title}</p>
+                        <ul>
+                            {note.info.todos.map((todo, index) => (
+                                <li key={index}>{todo.txt}</li> // Rendering the 'txt' property of each 'todo'
+                            ))}
+                        </ul>
+                    </React.Fragment>
+                );
+            default:
+                return <p>Unknown note type</p>;
+        }
+    };
+
+
     return (
         < article className="note-preview" >
             <h3>Note id: {note.id}</h3>
             <h3>Note type: {note.type}</h3>
+            {renderNoteContent()}
         </article >
     )
 }
