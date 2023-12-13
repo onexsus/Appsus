5e7209a0fdb05d6f494191c2a73f1eb1415312c4
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-13 21:30:59 +0200

    Update add new note form

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -9,16 +9,21 @@ export function NoteList({ notes }) {
     return <div>
         <h2>Note list</h2>
 
-        <div className="add-note-form">
+        <div className="add-note-form flex justify-center">
             <label htmlFor="note-content">Note content</label>
             <input type="text" id="note-content" placeholder="What's on your mind..." />
 
-            <select name="note-type" id="note-type">
-                <option value="text">text</option>
-                <option value="image">image</option>
-                <option value="video">video</option>
-                <option value="to-do">to-do</option>
-            </select>
+            <input type="radio" id="type-text" name="note-type" value="text" />
+            <label htmlFor="type-text">Text</label>
+
+            <input type="radio" id="type-image" name="note-type" value="image" />
+            <label htmlFor="type-image">Image</label>
+
+            <input type="radio" id="type-video" name="note-type" value="video" />
+            <label htmlFor="type-video">Video</label>
+
+            <input type="radio" id="type-todo" name="note-type" value="to-do" />
+            <label htmlFor="type-todo">To-Do</label>
         </div>
 
         <ul className="clean-list">
