1ce399c62032c3f91c8cf4bfdf8c58ebae33af10
onexsus <boriskho@gmail.com>
2023-12-17 07:44:18 +0200

    update

--- apps/mail/cmps/EmailFooter.jsx
+++ apps/mail/cmps/EmailFooter.jsx
@@ -6,7 +6,7 @@ export function EmailFooter(){
 
   return (
     <section className="main-footer flex column align-center">
-      <div className="footer-logo"><img src="../../../assets/img/bkmail-logo.jpg" alt="mail-logo" /></div>
+      <div className="footer-logo"><img src="./../../../assets/img/bkmail-logo.jpg" alt="mail-logo" /></div>
       <div className="footer-content">Create by team Boris Konstantin &copy;2023</div>
       <div className="footer-links"></div>
     </section>
